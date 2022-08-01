83b10422929e9df4c0677ccf0ac999a338ef78d5
dinger1986 <daniel.lamb@flonix.co.uk>
2022-08-01 17:11:23 +0100

    Create widnowsclientID.ps1

---
+++ widnowsclientID.ps1
@@ -0,0 +1,24 @@
+$ErrorActionPreference= 'silentlycontinue'
+# Get RustDesk ID
+
+If (!("C:\Windows\ServiceProfiles\LocalService\AppData\Roaming\RustDesk\config\RustDesk.toml")) {
+$username = ((Get-WMIObject -ClassName Win32_ComputerSystem).Username).Split('\')[1]
+$rustid=(Get-content C:\Users\$username\AppData\Roaming\RustDesk\config\RustDesk.toml | Where-Object { $_.Contains("id") })
+$rustid = $rustid.Split("'")[1]
+
+$rustpword = (Get-content C:\Users\$username\AppData\Roaming\RustDesk\config\RustDesk.toml | Where-Object { $_.Contains("password") })
+$rustpword = $rustpword.Split("'")[1]
+Write-output "Config file found in user folder"
+Write-output "$rustid"
+Write-output "$rustpword"
+}
+else {
+$rustid=(Get-content C:\Windows\ServiceProfiles\LocalService\AppData\Roaming\RustDesk\config\RustDesk.toml | Where-Object { $_.Contains("id") })
+$rustid = $rustid.Split("'")[1]
+
+$rustpword = (Get-content C:\Windows\ServiceProfiles\LocalService\AppData\Roaming\RustDesk\config\RustDesk.toml | Where-Object { $_.Contains("password") })
+$rustpword = $rustpword.Split("'")[1]
+Write-output "Config file found in windows service folder"
+Write-output "$rustid"
+Write-output "$rustpword"
+}
