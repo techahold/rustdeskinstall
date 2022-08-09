4f328e5863fbe8460ca4833d0ef1b15d1b22f130
dinger1986 <daniel.lamb@flonix.co.uk>
2022-08-09 22:36:18 +0100

    Update linuxclientinstall.sh

--- linuxclientinstall.sh
+++ linuxclientinstall.sh
@@ -8,34 +8,50 @@ if [[ $(which yum) ]]; then
 elif [[ $(which apt) ]]; then
    wget https://github.com/rustdesk/rustdesk/releases/download/1.1.9/rustdesk-1.1.9.deb
    sudo dpkg -i rustdesk-1.1.9.deb
-   sudo apt install -fy ./rustdesk-1.1.9.deb
+   sudo apt --fix-broken install -y
 else
    echo "Unknown Platform, the install will fail"
    exit
 fi
 
-su ${uname} -c "DISPLAY=:0 rustdesk --password ${admintoken}"
+rustdesk --password ${admintoken}
+pkill -f "rustdesk"
 
-# Setup Systemd to launch hbbs
-rustdesksignal="$(cat << EOF
-rendezvous_server = 'rs-ny.rustdesk.com'
+# Setup Rustdesk in user profile
+rustdesktoml2a="$(cat << EOF
+rendezvous_server = 'wanipreg'
 nat_type = 1
 serial = 3
 
 [options]
 rendezvous-servers = 'rs-ny.rustdesk.com,rs-sg.rustdesk.com,rs-cn.rustdesk.com'
-key = ''
-custom-rendezvous-server = ''
-api-server = 'https://'
-relay-server = ''
+key = 'keyreg'
+custom-rendezvous-server = 'wanipreg'
+api-server = 'https://wanipreg'
+relay-server = 'wanipreg'
 EOF
 )"
-echo "${rustdesksignal}" | sudo tee /home/${uname}/.config/rustdesk/RustDesk2.toml > /dev/null
+echo "${rustdesktoml2a}" | sudo tee /home/${uname}/.config/rustdesk/RustDesk2.toml > /dev/null
+
+# Setup Rustdesk in root profile
+rustdesktoml2b="$(cat << EOF
+rendezvous_server = 'wanipreg'
+nat_type = 1
+serial = 3
+
+[options]
+rendezvous-servers = 'rs-ny.rustdesk.com,rs-sg.rustdesk.com,rs-cn.rustdesk.com'
+key = 'keyreg'
+custom-rendezvous-server = 'wanipreg'
+api-server = 'https://wanipreg'
+relay-server = 'wanipreg'
+EOF
+)"
+echo "${rustdesktoml2b}" | sudo tee /root/.config/rustdesk/RustDesk2.toml > /dev/null
 
 chown ${uname}:${uname} /home/${uname}/.config/rustdesk/RustDesk2.toml 
-
-pkill -f "rustdesk"
 
 systemctl restart rustdesk
 
 cat /home/${uname}/.config/rustdesk/RustDesk.toml
+sudo cat /root/.config/rustdesk/RustDesk.toml
